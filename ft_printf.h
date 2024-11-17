d3d6b49d51c80895a2508b0bf61917c6824776ca
Nahil13ieme <nahil@Nahil.>
2024-11-17 03:55:33 +0100

    adding file descriptor

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nbenhami <nbenhami@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 18:34:22 by nbenhami          #+#    #+#             */
-/*   Updated: 2024/11/16 18:58:09 by nbenhami         ###   ########.fr       */
+/*   Updated: 2024/11/17 01:06:08 by nbenhami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 #include <stdarg.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int	ft_printf(const char *, ...);
-int	ft_putchar(char c);
-int	ft_putstr(char *str);
-int	ft_putnumber(int n);
-int	ft_putbase(char *base, char c);
+int	ft_putchar(char c, int fd);
+int	ft_putstr(char *str, int fd);
+int	ft_putnumber(int n, int fd);
+int	ft_putbase(char *base, char c, int fd);
 
 #endif
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,26 +6,51 @@
 /*   By: nbenhami <nbenhami@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 18:52:26 by nbenhami          #+#    #+#             */
-/*   Updated: 2024/11/16 19:05:20 by nbenhami         ###   ########.fr       */
+/*   Updated: 2024/11/17 01:08:48 by nbenhami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_putchar(char c)
+int	ft_putchar(char c, int fd)
 {
-	
+	write(fd, &c, sizeof(c));
 }
 
-int	ft_putstr(char *str)
+int	ft_putstr(char *str, int fd)
 {
-
+	write(fd, str, sizeof(str));
 }
 
-int	ft_putnumber(int n)
+int	ft_putnumber(int n, int fd)
 {
+	char	c;
 
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+	}
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	c = (n % 10) + '0';
+	write(fd, &c, 1);
 }
 
-int	ft_putbase(char *base, char c)
+int	ft_putbase(char *base, int n, int fd)
 {
-	
+	char	c;
+
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		n = -n;
+	}
+	if (n > 9)
+		ft_putnbr_fd(n / sizeof(base), fd);
+	c = base[n % 10];
+	write(fd, &c, 1);
 }
